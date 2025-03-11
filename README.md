a8ac2415813f2f09ca04f66198fde66814bf9125
aleksey <aleksey.work.job@gmail.com>
2025-03-11 12:07:15 +0300

    [doc] Документирование Комментирование

--- README.md
+++ README.md
@@ -1,5 +1,36 @@
-Описание кодов
+# Описание кодов
 
-Начальный скрипт Go `video.go`
+Начальный скрипт  `video.go`, `job.go`
 
-Тесты Go `video_test.go`
+Тесты  `video_test.go`, `job_test.go` - это еще примеры использования
+
+В файле `go.mod` описан корневой модуль `encoder` так `module encoder`
+
+Файлы `video.go`, `job.go` относятся к пакету `encoder/domain`
+Файл `db.go` относится к пакету `encoder/framework/database`
+Файлы `job_repository.go`, `video_repository.go` относятся к пакету `encoder/application/repositories`
+
+Использованные внешние библиотеки
+```GO
+require (
+	github.com/asaskevich/govalidator v0.0.0-20230301143203-a9d515a09cc2
+	github.com/jinzhu/gorm v1.9.16
+	github.com/lib/pq v1.1.1
+	github.com/satori/go.uuid v1.2.0
+	github.com/stretchr/testify v1.10.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+```
+
+
+
+
+
+
--- framework/database/db.go
+++ framework/database/db.go
@@ -4,26 +4,29 @@ import (
 	"encoder/domain"
 	"log"
 
-	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	_ "github.com/lib/pq"
+	"github.com/jinzhu/gorm"                   // ОРМ
+	_ "github.com/jinzhu/gorm/dialects/sqlite" //Sqlite
+	_ "github.com/lib/pq"                      //Postgresql
 )
 
+// Database Описание Баз Данных
 type Database struct {
-	Db            *gorm.DB
-	Dsn           string
-	DsnTest       string
-	DbType        string
-	DbTypeTest    string
-	Debug         bool
-	AutoMigrateDb bool
-	Env           string
+	Db            *gorm.DB //Ссылка на ORM
+	Dsn           string   //путь к БД прода
+	DsnTest       string   //путь к тестовой БД
+	DbType        string   //Тип базы прода
+	DbTypeTest    string   //Тип тестовой БД
+	Debug         bool     //Включать ли отладочный режим
+	AutoMigrateDb bool     //Делать ли автоматические миграции
+	Env           string   // test или prod
 }
 
+// NewDatabase Создать новую Базу Данных
 func NewDatabase() *Database {
 	return &Database{}
 }
 
+// NewDatabaseTest Новая тестовая База Данных в sqlite
 func NewDatabaseTest() *gorm.DB {
 	dbInstance := NewDatabase()
 	dbInstance.Env = "test"
@@ -41,25 +44,31 @@ func NewDatabaseTest() *gorm.DB {
 	return connection
 }
 
+// Connect Метод соединения с базой данных
 func (d *Database) Connect() (*gorm.DB, error) {
 	var err error
 
+	//Если задано окружение для тестовой базы используем тестовую базу, иначе используем базу для прода
 	if d.Env == "test" {
-		d.Db, err = gorm.Open(d.DbTypeTest, d.DsnTest)
+		d.Db, err = gorm.Open(d.DbTypeTest, d.DsnTest) //Открываем тестовую БД
 	} else {
-		d.Db, err = gorm.Open(d.DbType, d.Dsn)
+		d.Db, err = gorm.Open(d.DbType, d.Dsn) // Открываем БД прода
 	}
 
 	if err != nil {
 		return nil, err
 	}
 
+	// Включаем логи в отладочном режиме
 	if d.Debug {
 		d.Db.LogMode(true)
 	}
 
+	//Если автоматичесукие миграции включены, делаем миграции
 	if d.AutoMigrateDb {
+		//Делаем миграции Video и Job
 		d.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+		//Добавляем внешние ключи в БД
 		d.Db.Model(domain.Job{}).AddForeignKey("video_id", "videos(id)", "CASCADE", "CASCADE")
 	}
 
